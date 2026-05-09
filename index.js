1351586f597d5e84db699f10a354a5bb19b91589
Ychnightder <pierreychnightder561@gmail.com>
2026-05-09 17:51:30 +0200

    Auto-update: 2026-05-09T15:51:30.709Z

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ ${article.body}
 		console.log(`📝 Article sauvegardé avec image : ${fileName}`);
 
 		// --- AJOUT : DÉPLOIEMENT APRÈS LA GÉNÉRATION ---
-		//  await deploy();
+		 await deploy();
 	} catch (error) {
 		console.error('💀 Pipeline crash:', error);
 	}
