27fb807223c70207ea3bec0a9af0eedfe15ae009
Ychnightder <pierreychnightder561@gmail.com>
2026-05-09 02:01:28 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, './.env') });
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss(), '@tailwindcss/forms'], // Ajout du plugin forms de Tailwind
 	},
+	site: process.env.SITE_URL || '',
+	integrations: [sitemap()],
 });
