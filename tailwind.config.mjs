78f1f1d65fb6f5c58a90ecce97d1dada6bc8e6de
Ychnightder <pierreychnightder561@gmail.com>
2026-05-12 22:45:13 +0200

    pinterest-rich-pin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,1 +1,9 @@
-plugins: [require('@tailwindcss/typography')]
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdoc,svelte,ts,tsx,vue}'],
+	darkMode: 'class', 
+	theme: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
