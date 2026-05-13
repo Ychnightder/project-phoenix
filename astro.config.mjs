803fd415c7e962965088b02dfaa9dab861f91416
Ychnightder <pierreychnightder561@gmail.com>
2026-05-13 15:02:04 +0200

    vercel inmport

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,22 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 export default defineConfig({
 	// Utilise une valeur par défaut en dur si le .env est vide pour éviter les erreurs de build
 	site: process.env.SITE_URL || 'https://phenix-blog.vercel.app',
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	integrations: [sitemap()],
+	adapter: vercel({
+		webAnalytics: { enabled: true }, // Optionnel
+	}),
+	output: 'hybrid',
 });
