d97dcf7635e0eef19a80897750936b285f176913
Ychnightder <pierreychnightder561@gmail.com>
2026-05-13 15:57:51 +0200

    correction server vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,20 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 
+import vercel from '@astrojs/vercel';
+
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 export default defineConfig({
-	// Utilise une valeur par défaut en dur si le .env est vide pour éviter les erreurs de build
-	site: process.env.SITE_URL || 'https://phenix-blog.vercel.app',
+  // Utilise une valeur par défaut en dur si le .env est vide pour éviter les erreurs de build
+  site: process.env.SITE_URL || 'https://phenix-blog.vercel.app',
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	integrations: [sitemap()],
-
+  integrations: [sitemap()],
+  adapter: vercel(),
 });
