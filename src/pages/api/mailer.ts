0154091636a362350438ae1580b14d912f8aa960
Ychnightder <pierreychnightder561@gmail.com>
2026-05-15 01:58:15 +0200

    new import

--- src/pages/api/mailer.ts
+++ src/pages/api/mailer.ts
@@ -1,7 +1,7 @@
 import { db } from '../../lib/db'
 import nodemailer from 'nodemailer';
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import matter from 'gray-matter'; 
 
 async function phoenixAgent() {
