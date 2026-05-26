71d072db0a850201e405cf24cc2c2f706149e9a7
Ychnightder <pierreychnightder561@gmail.com>
2026-05-26 02:53:34 +0200

    restricted email sent to subscribers

--- index.js
+++ index.js
@@ -65,9 +65,11 @@ ${article.body}
 		// --- AJOUT : DÉPLOIEMENT APRÈS LA GÉNÉRATION ---
 		await deploy();
 
-		console.log('📧 Lancement de la diffusion newsletter...');
+		if(Math.random() < 0.5) {
+			console.log('📧 Lancement de la diffusion newsletter...');
 
-		 await phoenixAgent();
+			await phoenixAgent();
+		}
 	} catch (error) {
 		await deploy();
 		console.error('💀 Pipeline crash:', error);
